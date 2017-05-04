a9a58c15d3970c70e15fb9b415844a8db938d624
SMuyesser <smuyesser@gmail.com>
2017-05-04 15:31:31 -0700

    Adds server.js

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const morgan = require('morgan');
+
+const app = express();
+
+const getPostRouter = require('./getPostRouter');
+const deletePutRouter = require('./deletePutRouter');
+
+app.use(morgan('common'));
+
+app.use(express.static('public'));
+
+app.use('/blog-posts', getPostRouter);
+app.use('/blog-posts/:id', deletePutRouter);
+
+app.listen(process.env.PORT || 8080, () => {
+	console.log(`Your app is listening on port ${porcess.env.PORT || 8080}`);
+});
